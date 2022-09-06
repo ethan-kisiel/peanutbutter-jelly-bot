67aeb46c347b1d8dc3418d2267b57859c7ff18e3
ethan-kisiel <ethan.a.kisiel@gmail>
2022-09-06 17:12:12 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import praw
+import environment_vars
+from time import sleep
+from os import environ as env
+
+check_for = ["peanutbutterandjelly",
+             "peanutbutter&jelly",
+             "pbandj",
+             "pb&j",
+             "peanutbutterjellytime"]
+
+link = "[It's peanut butter jelly time!](https://www.youtube.com/watch?v=OOIJxKopZFQ)"
+
+def main():
+    reddit = praw.Reddit(client_id=env.get("CLIENT_ID"),
+                         client_secret=env.get("CLIENT_SECRET"),
+                         user_agent=env.get("USER_AGENT"),
+                         username=env.get("USERNAME"),
+                         password=env.get("PASSWORD"))
+
+    subreddit = reddit.subreddit("all")
+    for submission in subreddit.hot(limit=1):
+        for comment in submission.comments:
+            if hasattr(comment, "body"):
+                condensed_lower = comment.body.lower().replace(' ', '')
+                for check in check_for:
+                    if condensed_lower.contains(check):
+                        # respond with the text
+                        comment.reply(link)
+                        sleep(660)
+                        break
+    return
+
+if __name__ == "__main__":
+    main()
