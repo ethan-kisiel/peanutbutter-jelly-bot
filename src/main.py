4862222ed577befc4a858fc6c37249c9b3a13527
ethan-kisiel <ethan.a.kisiel@gmail.com>
2022-09-09 09:24:38 -0400

    finalized main function

--- src/main.py
+++ src/main.py
@@ -1,23 +1,31 @@
 from time import sleep
 from pbj_bot import Bot
+def min_to_seconds(minutes: int) -> int:
+    return minutes * 60
 
 def main():
     """
-    Perform scan on r/all every hour
+    Perform scan on r/all every half hour
     after each scan, drain bad replies
     """
     test_bot = Bot()
-    print(test_bot)
-    '''
-    scan_result = test_bot.perform_scan("peanutbutterjellybot")
+    while test_bot:
+        try:
+            # perform a scan on top 15 posts of all
+            scan_result = test_bot.perform_scan("all", 15)
+            if not scan_result:
+                print("Successfully exited scan.")
+            else:
+                print("Scan exited with non-zero code.")
 
-    if not scan_result:
-        pass
-    else:
-        pass
-    test_bot.drain_bad_replies()
-    '''
+            sleep(min_to_seconds(30))
+            test_bot.drain_bad_replies()
+        except:
+            print("uh oh... Something bad is happening!")
+            break
+
     return
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
