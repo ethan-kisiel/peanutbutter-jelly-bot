a94d69957c03da7f52b901bd3538d5c3e2e9540a
ethan-kisiel <ethan.a.kisiel@gmail.com>
2022-09-09 08:42:08 -0400

    added Bot class import to pbj_bot/__init__.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
 from time import sleep
-from pbj_bot import bot
+from pbj_bot import Bot
 
 def main():
     """
     Perform scan on r/all every hour
     after each scan, drain bad replies
     """
-    test_bot = bot.Bot()
+    test_bot = Bot()
     print(test_bot)
+    '''
     scan_result = test_bot.perform_scan("peanutbutterjellybot")
+
     if not scan_result:
         pass
     else:
         pass
     test_bot.drain_bad_replies()
-
+    '''
     return
 
 if __name__ == "__main__":
